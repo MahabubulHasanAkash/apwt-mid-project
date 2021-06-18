7777c04d2013b003e91b1029dbad03402cfe999c
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2021-06-19 00:13:06 +0600

    views

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
+Route::get('/crator/home', function () {
     return view('creator.home');
 });
 
