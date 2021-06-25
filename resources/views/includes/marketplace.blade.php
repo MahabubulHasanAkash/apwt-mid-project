c9834b68b0319d71269627a005ee3aae2c2d862b
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2021-06-25 21:50:03 +0600

    adminHome

---
+++ resources/views/includes/marketplace.blade.php
@@ -0,0 +1,36 @@
+<div>
+    <div class="container-fluid">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+          </div>
+    </div>
+    <div class="cotainer-fluid" style="margin-left:5%">
+        <div class="row">
+            <div class="col-md-4" style="height:300px;width:300px">
+                <div class="thumbnail">
+                    <a href="/details" {{--put link for next page--}} target="_blank"> 
+                    <img src="https://cdn.pixabay.com/photo/2017/11/20/02/00/fantasy-2964231_960_720.jpg" alt="Lights" style="height:220px;width:300px">
+                        <div class="caption">
+                            <p>Title 1</p>
+                        </div>
+                    </a>
+                </div>
+            </div>    
+            
+            
+        </div>
+    </div>
+</div>
